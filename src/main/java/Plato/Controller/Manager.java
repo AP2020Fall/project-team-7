90136d576260f1a88a26dd1fa8f3f632946c6b1c
sarazm2000 <sarazm.2000@gmail.com>
2020-12-14 17:04:11 +0330

    Main and Manager

--- src/main/java/Plato/Controller/Manager.java
+++ src/main/java/Plato/Controller/Manager.java
@@ -1,10 +1,15 @@
 package Plato.Controller;
+import Plato.Model.Event;
+import Plato.Model.Game;
 import Plato.Model.Player;
+import org.ietf.jgss.GSSManager;
+
 import java.util.ArrayList;
 
 public class Manager {
-    Player player = null;
     private ArrayList<Player> allUsers;
+    private ArrayList<Game> games;
+    private ArrayList<Event> events;
 
 
     public ArrayList<Player> getAllUsers() {
@@ -18,5 +23,21 @@ public class Manager {
     public void addPlayer(Player player){
         allUsers.add(player);
         setAllUsers(allUsers);
+    }
+
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
     }
 }
--- src/main/java/Plato/View/Main.java
+++ src/main/java/Plato/View/Main.java
@@ -1,10 +1,14 @@
 package Plato.View;
 
+import Plato.Controller.Manager;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Manager manager = new Manager();
+        CommandProcessor commandProcessor = new CommandProcessor(manager);
+        commandProcessor.run();
 
     }
 
