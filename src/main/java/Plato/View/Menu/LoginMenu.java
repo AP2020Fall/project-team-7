4d62e76899c53d710baf5e77a387edde280968cd
sarazm2000 <sarazm.2000@gmail.com>
2020-12-16 21:29:22 +0330

    some changes in LoginMenu

--- src/main/java/Plato/View/Menu/LoginMenu.java
+++ src/main/java/Plato/View/Menu/LoginMenu.java
@@ -1,8 +1,14 @@
 package Plato.View.Menu;
 
+import java.util.HashMap;
+
 public class LoginMenu extends Menu{
     public LoginMenu(Menu parentMenu) {
         super("login", parentMenu);
+        HashMap<Integer, Menu> submenus = new HashMap<>();
+        submenus.put(1, loginToAccount());
+        this.setSubmenus(submenus);
+
     }
 
     private Menu loginToAccount() {
@@ -10,7 +16,7 @@ public class LoginMenu extends Menu{
             @Override
             public void show() {
                 System.out.println(this.getName() + ":");
-                System.out.println("If you have not an account creat one!\n" +
+                System.out.println("If you have not an account create one!\n" +
                         "Enter username and password:" +
                         "\nEnter back to return");
             }
