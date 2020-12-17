69d86e2b334b15fdae144479cf03bb382aa9163f
sarazm2000 <sarazm.2000@gmail.com>
2020-12-17 12:50:15 +0330

    GameMenu

--- src/main/java/Plato/View/Menu/GameMenu.java
+++ src/main/java/Plato/View/Menu/GameMenu.java
@@ -1,4 +1,36 @@
 package Plato.View.Menu;
 
-public class GameMenu {
+import java.util.HashMap;
+
+public class GameMenu extends Menu{
+    public GameMenu(String name, Menu parentMenu) {
+        super(name, parentMenu);
+        HashMap<Integer, Menu> submenus = new HashMap<>();
+        submenus.put(1, showGame());
+        this.setSubmenus(submenus);
+    }
+
+    private Menu showGame() {
+        return new Menu("Games", this) {
+            @Override
+            public void show() {
+                System.out.println(this.getName() + ":");
+                System.out.println("enter username and password");
+                System.out.println("if you have not account back and register!");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                } else {
+
+                    this.show();
+                    this.execute();
+                }
+            }
+        };
+    }
 }
