2f9f24edf497de8a973502981d5e29fe7474779e
sarazm2000 <sarazm.2000@gmail.com>
2021-01-11 13:24:19 +0330

    view profile info

--- src/main/java/Plato/Model/Admin.java
+++ src/main/java/Plato/Model/Admin.java
@@ -11,11 +11,11 @@ public class Admin extends Person {
         super(firstName, lastName, username, id, password, email, phoneNumber);
     }
 
-    private void addEvent(Game gameName, Date start, Date finish, int score){
+    private void addEvent(Game gameName, Date start, Date finish, int score) {
         Event event = new Event(gameName, start, finish, score);
     }
 
-    private void deleteEvent(Event event){
+    private void deleteEvent(Event event) {
 
     }
 
@@ -25,5 +25,17 @@ public class Admin extends Person {
 
     public void setAdminSuggestion(ArrayList<String> adminSuggestion) {
         this.adminSuggestion = adminSuggestion;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" + "\n" +
+                "firstName='" + firstName + '\'' + "\n" +
+                ", lastName='" + lastName + '\'' + "\n" +
+                ", username='" + username + '\'' + "\n" +
+                ", Id='" + Id + '\'' + "\n" +
+                ", Email='" + Email + '\'' + "\n" +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
     }
 }
