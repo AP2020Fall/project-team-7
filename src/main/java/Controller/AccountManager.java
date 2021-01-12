1cf16f5e640b034d78022fcd885354dc3ee30493
sarazm2000 <sarazm.2000@gmail.com>
2021-01-13 00:35:38 +0330

    Login user

--- src/main/java/Controller/AccountManager.java
+++ src/main/java/Controller/AccountManager.java
@@ -5,6 +5,10 @@ import View.AccountMenu;
 public class AccountManager extends Manager {
 
     public AccountManager() {
+//        menu = new AccountMenu(this);
+        execute();
+    }
+    public void execute(){
         menu = new AccountMenu(this);
     }
 }
--- src/main/java/View/LoginMenu.java
+++ src/main/java/View/LoginMenu.java
@@ -1,21 +1,32 @@
 package View;
 
+import Controller.AccountManager;
 import Controller.LoginManager;
 import Controller.Manager;
 
-public class LoginMenu extends Menu{
+public class LoginMenu extends Menu {
     public LoginMenu(Manager manager) {
         super(manager);
         System.out.println("username:");
         String username = scanner.nextLine();
         System.out.println("password:");
         String password = scanner.nextLine();
-        while (!((LoginManager) manager).loginUser(username, password)){
-            System.err.println("username or password is wrong! try again!");
-            System.out.println("username:");
-            username = scanner.nextLine();
-            System.out.println("password:");
-            password = scanner.nextLine();
+        while (!((LoginManager) manager).loginUser(username, password)) {
+            System.err.println("username or password is wrong! try again or back to register!");
+            if (username.equalsIgnoreCase("back")) {
+                ((AccountManager) manager).execute();
+
+            } else {
+                System.out.println("username:");
+                username = scanner.nextLine();
+            }
+            if (password.equalsIgnoreCase("back")) {
+                ((AccountManager) manager).execute();
+
+            } else {
+                System.out.println("password:");
+                password = scanner.nextLine();
+            }
         }
         System.out.println("welcome " + username);
     }
