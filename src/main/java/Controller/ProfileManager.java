75be3ff5932695ab23ef5567bb24a4273841bf5d
sarazm2000 <sarazm.2000@gmail.com>
2021-01-13 17:02:47 +0330

    develop Profile

--- src/main/java/Controller/ProfileManager.java
+++ src/main/java/Controller/ProfileManager.java
@@ -1,10 +1,26 @@
 package Controller;
 
+import Model.Player;
 import View.Profile;
 
-public class ProfileManager extends Manager
-{
+public class ProfileManager extends Manager {
     public ProfileManager() {
         menu = new Profile(this);
+    }
+
+    public void   showPersonalInfo() {
+        System.out.println(currentPlayer.toString());
+    }
+
+    public boolean canDeleteAccount(String username, String password) {
+        boolean canDelete = false;
+        for (Player player : players) {
+            if (player.getUsername().equals(username) && player.getPassword().equals(password)) {
+                canDelete = true;
+                players.remove(player);
+                break;
+            }
+        }
+        return canDelete;
     }
 }
--- src/main/java/View/Profile.java
+++ src/main/java/View/Profile.java
@@ -1,19 +1,67 @@
 package View;
 
 import Controller.Manager;
+import Controller.ProfileManager;
 
 public class Profile extends Menu {
     public Profile(Manager manager) {
         super(manager);
-        System.out.println("Enter 1 to go to Main page!" +
-                "plato age: " + manager.getCurrentPlayer().getPlatoAge() + "\n" +
-                "friends: " + manager.getCurrentPlayer().getFriendList().size() + "\n" +
-                "score: " + manager.getCurrentPlayer().getScore() + "\n" +
-                "1. Main page");
-        String input = "";
-        while (!(input = scanner.nextLine()).matches("^1$")){
-            System.err.println("invalid input! enter 1 to go Main page:");
-        }
-        returnMainPage();
+        System.out.println("Enter 3 to go to Main page!" +
+                "username: " + "\n" +
+                "plato age: " + 0 + "\n" +
+                "friends: " + 0 + "\n" +
+                "score: " + 0 + "\n" +
+                "1. view personal info\n" +
+                "2. delete Account\n" +
+                "3. Main page");
+        while (true) {
+            String input = scanner.nextLine();
+//            if ((!input.matches("^1$")) || (!input.matches("^2$")) || (!input.matches("^3$"))) {
+//                System.err.println("invalid input! enter back to return profile or 3 to go Main page:");
+//                continue;
+//            } else {
+                if (input.matches("^1$")) {
+                    System.out.println("view personal info:");
+                    ((ProfileManager) manager).showPersonalInfo();
+                    System.out.println("enter back to return profile or enter 3 to back to Main page: ");
+                    input = scanner.nextLine();
+                    while (!input.matches("^back$ || ^3$")){
+                        System.out.println("enter back to return profile or enter 3 to back to Main page: ");
+                        input = scanner.nextLine();
+                    }
+                    if (input.equalsIgnoreCase("back")) {
+                        goProfile();
+                    } else if (input.matches("^3$")) {
+                        returnMainPage();
+                    }
+                    break;
+                } else if (input.matches("^2$")) {
+                    System.out.println("delete account:");
+                    System.out.println("username:");
+                    String username = scanner.nextLine();
+                    if (username.equalsIgnoreCase("back")) {
+                        goProfile();
+                    }
+                    System.out.println("password:");
+                    String password = scanner.nextLine();
+                    while (!((ProfileManager) manager).canDeleteAccount(username, password)) {
+                        System.err.println("username or password is wrong!");
+                        System.out.println("username:");
+                        username = scanner.nextLine();
+                        if (username.equalsIgnoreCase("back")) {
+                            goProfile();
+                        }
+                        System.out.println("password:");
+                        password = scanner.nextLine();
+                        ((ProfileManager) manager).canDeleteAccount(username, password);
+                    }
+                    System.out.println("account " + username + " deleted!");
+                    break;
+
+                } else {
+                    returnMainPage();
+                }
+            }
+//        }
     }
 }
