8512a1889c50163d7882a2c72268901911507592
sarazm2000 <sarazm.2000@gmail.com>
2021-01-16 23:41:55 +0330

    add friend: bug

--- src/main/java/Controller/MainPageManager.java
+++ src/main/java/Controller/MainPageManager.java
@@ -77,7 +77,7 @@ public class MainPageManager extends Manager {
         for (Player player : getPlayers()) {
             if (player.getUsername().equals(username)) {
                 player.requestResult(currentPlayer, accept);
-                player.addFriend(currentPlayer);
+                addFriend(player);
                 break;
             }
         }
